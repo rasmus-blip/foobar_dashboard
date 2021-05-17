cc877e0965b899747612da54ab9eae327f1ac213
Mathias <math92k4@stud.kea.dk>
2021-05-17 12:51:24 +0200

    initial setup for displaying and updating personel

--- main.js
+++ main.js
@@ -7,64 +7,113 @@ import { getJSON } from "./rest_actions.js";
 window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-  let newData;
-  let oldData;
-
   const url = "https://foobarfirefjerdedele.herokuapp.com/";
-  newData = await getJSON(url);
+  let newData = await getJSON(url);
+  let oldData = [];
 
   displayDataInit(newData);
 
-  setInterval(updateData, 5000);
+  setInterval(updateDataArrays, 2000);
 
-  async function updateData() {
+  async function updateDataArrays() {
     oldData = newData;
     newData = await getJSON(url);
 
-    const dataToUpdate = compareData(oldData, newData);
-    console.log(dataToUpdate.length);
-    if (dataToUpdate.length > 0) {
-      updateUI(dataToUpdate);
-    }
+    determineDataToUpdate(newData, oldData);
   }
 }
 
 function displayDataInit(data) {
-  const peter = document.querySelector("#Peter");
-  const dannie = document.querySelector("#Dannie");
-  const jonas = document.querySelector("#Jonas");
+  displayBartenders(data.bartenders);
 
-  data.bartenders.forEach((person) => {
-    if (person.name === "Peter") {
-      peter.querySelector(".using_tap").textContent = person.usingTap;
+  //TODO ny function for hvert objekt
+}
+function displayBartenders(bartenders) {
+  const container = document.querySelector("#personel ul");
+  const temp = document.querySelector("#bartenders_temp");
+  console.log(bartenders);
+
+  bartenders.forEach((bartender) => {
+    const klon = temp.cloneNode(true).content;
+
+    klon.querySelector("li").dataset.id = bartender.name.toLowerCase();
+    klon.querySelector("h2").textContent = bartender.name;
+
+    if (bartender.status === "WORKING") {
+      klon.querySelector(".status span").textContent = getDetailText(bartender);
+    } else {
+      klon.querySelector(".status span").textContent = "Ready";
+      klon.querySelector(".status span").classList.add("green");
     }
+
+    container.appendChild(klon);
+
+    console.log(klon);
   });
 }
+function getDetailText(bartender) {
+  if (bartender.statusDetail === "pourBeer") {
+    return `Pouring beer on tap #${bartender.usingTap}, for order #${bartender.servingCustomer}`;
+  }
 
-function compareData(oldData, newData) {
-  let result;
+  if (bartender.statusDetail === "releaseTap") {
+    return `Released tap, order #${bartender.servingCustomer}`;
+  }
 
-  oldData.bartenders.forEach((oldObject) => {
-    result = newData.bartenders.filter(comparingBartenders);
+  if (bartender.statusDetail === "receivePayment") {
+    return `Receiving payment from order #${bartender.servingCustomer}`;
+  }
 
-    function comparingBartenders(newObject) {
-      if (JSON.stringify(oldObject) !== JSON.stringify(newObject) && newObject.name === oldObject.name) {
+  if (bartender.statusDetail === "startSercing") {
+    return `Preparing order #${bartender.servingCustomer}`;
+  }
+}
+
+function determineDataToUpdate(newData, oldData) {
+  const bartendersToUpdate = getDataToUpdate(newData.bartenders, oldData.bartenders, "name");
+  updateBartenders(bartendersToUpdate);
+}
+
+function getDataToUpdate(newData, oldData, id) {
+  const result = [];
+
+  oldData.forEach((oldObject) => {
+    const objectToUpdate = newData.filter(compareData);
+
+    function compareData(newObject) {
+      if (JSON.stringify(oldObject) !== JSON.stringify(newObject) && oldObject[id] === newObject[id]) {
         return true;
       } else {
         return false;
       }
+    }
+
+    if (objectToUpdate[0] !== undefined) {
+      result.push(objectToUpdate[0]);
     }
   });
 
   return result;
 }
 
-function updateUI(data) {
-  console.log("update");
+function updateBartenders(bartenders) {
+  bartenders.forEach((bartender) => {
+    const container = document.querySelector(`#personel [data-id="${bartender.name.toLowerCase()}"]`);
 
-  data.forEach((object) => {
-    const person = document.querySelector("#" + object.name);
-    person.querySelector(".using_tap").textContent = object.usingTap;
-    console.log(object.usingTap);
+    container.querySelector(".status span").textContent = getDetailText(bartender);
   });
 }
+
+function getDataToAppend() {}
+function getDataToRemove() {}
+
+//inital displaying UI;
+// sammenligner new med old - dvs vi får alt dataen
+// append
+
+//update UI
+//sammenligner new med old - dvs vi får noget af dataen
+//append
+//sammeligner old med med
+//remove
+//Find elementer der staig eksisterer men har ændret sig (brug data-set)
